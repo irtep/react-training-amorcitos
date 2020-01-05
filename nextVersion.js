a1d05256617c28b90b56bb41071340a696a7d648
irtep <rasanen.petri@gmail.com>
2020-01-05 21:35:39 +0200

    1 file added

---
+++ nextVersion.js
@@ -0,0 +1,12 @@
+/*
+components:
+-background
+  renders stars to background
+-header
+ as now
+-menu bars
+  mapping the menu bars
+-center page
+  by start, clock and queens photo
+  when clicking the menu bars, mapping the items
+*/ 
