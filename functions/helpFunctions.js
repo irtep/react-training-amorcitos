efb0e0293acb9d431a7fa62c03d242eecc5d1e98
irtep <rasanen.petri@gmail.com>
2020-01-08 04:59:43 +0200

    2 files modified

--- functions/helpFunctions.js
+++ functions/helpFunctions.js
@@ -1,6 +1,28 @@
+import { mainHeaders } from '../arrays/arrays.js';
 
 // calls random dice, for example 6 is 1d6
 export function callDice(max){
     const result =  1 + Math.floor(Math.random() * max);
     return result;
 } 
+
+// makes link list
+export function makeLinkMenu(targetMenu) {
+
+
+/* refs:
+
+    name: "Entertainment",
+    links: [
+      { name: "Diario", url: "http://irtep.net/diario" },
+
+  targetState.map(selectedMenu => {
+            return (                      
+              <LinksItem
+                key ={selectedMenu.id}
+                title ={selectedMenu}
+                handleClick ={this.clickControl}/>
+            )
+          })
+*/ 
+}
