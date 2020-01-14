2997b9782d9bcfb0f22ed6e5ae3bdb2eaf1b81ef
irtep <rasanen.petri@gmail.com>
2020-01-14 10:52:52 +0200

    1 file modified

--- components/LinkItem.js
+++ components/LinkItem.js
@@ -9,13 +9,12 @@ class LinkItem extends Component {
 
   goToUrl() {
     const win = window.open(this.props.url, "_blank");
-
     win.focus();
   }
 
   render() {
     return (
-      <div className="linksMenu allLinks" onClick={() => this.goToUrl()}>
+      <div className="linksMenu allLinks" onClick={this.goToUrl}>
         <i className="fa fa-caret-right" />{" "}
         <span id="linkTexts">
           {this.props.title} <br />
